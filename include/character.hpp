a2bed8179d56fe1f673a65ddb416e92df59eaf7e
Felipe Diniz <felipeard2006@gmail.com>
2026-01-23 16:12:11 -0300

    Finished the character class declaration

--- include/character.hpp
+++ include/character.hpp
@@ -19,11 +19,11 @@ class Character {
         std::vector<Item*> inventory;
         std::set<std::string> abilities;
     public:
-        Character(std::string name, std::string characterClass);
+        Character(const std::string& name, const std::string& characterClass);
 
         // Getters
-        std::string getName() const;
-        std::string getCharacterClass() const;
+        const std::string& getName() const;
+        const std::string& getCharacterClass() const;
         int getMaxHP() const;
         int getCurrentHP() const;
         int getLevel() const;
@@ -31,18 +31,43 @@ class Character {
         int getDamage() const;
         int getDefense() const;
         std::tuple<int, int> getPosition() const;
-        std::vector<Item*> getInventory() const;
-        std::set<std::string> getAbilities() const;
+        const std::vector<Item*>& getInventory() const;
+        const std::set<std::string>& getAbilities() const;
 
         // Setters 
         void setName(const std::string& name);
         void setCharacterClass(const std::string& characterClass);
-        void setMaxHP(const int maxHP);
-        void setCurrentHP(const int currentHP);
-        void setLevel(const int level);
-        void setXP(const int XP);
-        void setDamage(const int damage);
-        void setDefense(const int defense);        
+        void setMaxHP(int maxHP);
+        void setCurrentHP(int currentHP);
+        void setLevel(int level);
+        void setXP(int XP);
+        void setDamage(int damage);
+        void setDefense(int defense);        
+
+        // Inventory methods
+        void addItem(Item* item);
+        void removeItem(const std::string& itemName);
+        void clearInventory();
+        const Item* getItem(int index) const;
+        int getInventorySize() const;
+        void showInventory() const;
+
+        // Abilities methods
+        bool hasAbility(const std::string& ability) const;
+        void showAbilities() const;
+        bool learnAbility(const std::string& ability); 
+        int getAbilitiesCount() const;
+
+        //Position methods 
+        void move(int deltaX, int deltaY);
+        void moveNorth();
+        void moveSouth();
+        void moveEast();
+        void moveWest();
+
+        // Other methods
+        void display() const;
+
 };
 
 #endif // Character_HPP
