8db2dbb27d3b9fa104d0c09515064069d25f77bc
Felipe Diniz <felipeard2006@gmail.com>
2026-01-24 00:03:22 -0300

    Advanced on menu development

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <string>
+#include <algorithm>
 // #include "../include/item.hpp"
 #include "../include/character.hpp"
+
+std::string readName() {
+    std::string name;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Enter the character's name: ";
+    std::getline(std::cin, name);
+    return name;
+}
+
+void printMenu() {
+        std::cout << "\n========================================\n";
+    std::cout << "        JUST A BASIC RPG GAME        \n";
+    std::cout << "========================================\n";
+    std::cout << " 1. Create a new character\n";
+    std::cout << " 2. Display character stats\n";
+    std::cout << " 3. \n";
+    std::cout << " 4. \n";
+    std::cout << " 5. \n";
+    std::cout << " 6. \n";
+    std::cout << " 7. \n";
+    std::cout << " 8. \n";
+    std::cout << " 9. Exit\n";
+    std::cout << "========================================\n";
+    std::cout << "Please, choose an option: ";
+}
+
+void enterToContinue() {
+    std::cout << "Press enter to continue...";
+    std::cin.get();
+}
+
+
+bool nameValidation(const std::string& name) {
+    
+    if(name.length() == 0) {
+        std::cout << "You should enter an non-empty name. \n";
+        return false;
+    }
+    bool onlySpaces = std::all_of(
+        name.begin(), name.end(),
+        [](unsigned char c) { return std::isspace(c); }
+    );
+
+    if(onlySpaces) {
+        std::cout << "You should enter a proper name. Name cannot contain only spaces. \n";
+        return false;
+    }   
+
+    return true;
+}
 
 std::unique_ptr<Character> createCharacter(const std::string& name) {
     std::cout << "Choose your character class: \n";
@@ -33,45 +85,119 @@ std::unique_ptr<Character> createCharacter(const std::string& name) {
     }
 }
 
-void showCharacterStats (const std::unique_ptr<Character>& character) {
-    std::cout << "================================" << "\n";
-    std::cout << character->getName() << "'s stats:" "\n";
-    std::cout << "Combat class > " << character->getCharacterClass() << "\n";
-    std::cout << "Health > " << character->getCurrentHP() << "/" << character->getMaxHP() << "\n";
-    std::cout << "Level > " << character->getLevel() << "\n";
-    std::cout << "Experience > " << character->getXP() << "\n";
-    std::cout << "Damage > " << character->getDamage() << "\n";
-    std::cout << "Defense > " << character->getDefense() << "\n";
-    character->showInventory();
-    std::cout << "================================" << "\n";
+void showPartyMembers(std::vector<std::unique_ptr<Character>>& party) {
+    std::cout << "Party Members:\n";
+    for(const std::unique_ptr<Character>&  character : party) {
+        std::cout << character->getName() << " (" << character->getCharacterClass() << ")\n";
+    }
 }
 
+void showCharacterStats(std::string& name, std::vector<std::unique_ptr<Character>>& party) {
+    for(const std::unique_ptr<Character>&  character : party) {
+
+        if(name != character->getName()) {
+            std::cout << "This party member doesn't exist.\n";
+            return;
+        }
+
+        if(name == character->getName()) {
+            character->display();
+            return;
+        }
+    }
+}
+
+bool partyHasMember(const std::vector<std::unique_ptr<Character>>& party) {
+    if(party.empty()) {
+        std::cout << "The party is empty. \n";
+        enterToContinue();
+        return false;
+    }
+
+    return true;
+}
+
+bool characterIsAlreadyInParty(std::string& name, std::vector<std::unique_ptr<Character>>& party) {
+    for(const std::unique_ptr<Character>&  character : party) {
+        if(name == character->getName()) {
+            std::cout << "Character already exists. \n";
+            enterToContinue();
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
 int main() {
-
-    std::unique_ptr<Character> c1 = createCharacter("Aragorn");
-    showCharacterStats(c1);
-
-
-    // std::cout << c1->getPosition() << "\n";
-
-
-    // std::cout << c1->getAbilities() << "\n";
+    std::vector<std::unique_ptr<Character>> party;
     
-    /*
-    Item item("heal Potion", "Item", 5, "Heals the user.");
 
-    std::cout << item.getName() << std::endl;
-    std::cout << item.getType() << std::endl;
-    std::cout << item.getPrice() << std::endl;
-    std::cout << item.getDescription() << std::endl;
-    item.display();
-            if (!(std::cin >> option)) {
+     do {
+        int option;
+
+        printMenu();
+
+        if (!(std::cin >> option)) {
             std::cout << "Invalid input. Please enter a number.\n";
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             continue;
         }
-    */
+
+        switch (option) {
+            case 1: {
+                std::string name = readName();
+                if(!nameValidation(name)) break;
+                if(characterIsAlreadyInParty(name, party)) break;
+                std::unique_ptr<Character> character = createCharacter(name);
+                party.push_back(std::move(character));
+                std::cout << "Character created successfully!\n";
+                enterToContinue();
+                break;
+            }
+            case 2: {
+                if(!partyHasMember(party)) break; 
+                showPartyMembers(party);
+                std::string name = readName();
+                if(!nameValidation(name)) break;
+                showCharacterStats(name, party);
+                enterToContinue();
+                break;
+            }
+            case 3: {
+                enterToContinue();
+                break;
+            }
+            case 4: {
+                enterToContinue();
+                break;
+            }
+            case 5: {
+                enterToContinue();
+                break;
+            }
+            case 6: {
+                enterToContinue();
+                break;
+            }
+            case 7: {
+                enterToContinue();
+                break;
+            }
+            case 8: {
+                enterToContinue();
+                break;
+            }
+            case 9: 
+                std::cout << "Shutdowning down..." << std::endl;
+                return 0;
+            default:
+                std::cout << "Please, choose a valid option." << std::endl;
+                break;
+        }
+    } while(true);
 
     return 0;
 }
